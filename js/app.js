ff4d3b9988dfdfe3d34be9769cd90c9e1697553f
Matthew Lamar Kerns <matthewkerns@ymail.com>
2016-04-15 14:00:33 -0400

    Added end game functionality

--- js/app.js
+++ js/app.js
@@ -3,30 +3,11 @@ var speedNumber = 1;
 var count = 0;
 
 function gameOver() {
-    alert("Congratulations, You Win!")
+    alert("Congratulations, You Win!");
+    document.write("Game Over");
 };
 
 $("#count").text(count);
-
-function updateCount(str, player) {
-  console.log(player);
-  if(str == "pass") {
-    console.log("Passed count before is " + count);
-    count++;
-    console.log("Passed count after is " + count);
-    count < 15 ? player.resetPlayer() : gameOver();
-  }
-  else if(str == "fail") {
-    count--;
-    player.resetPlayer();
-  }
-  else {
-    //nothing
-  }
-
-  $("#count").text(count);
-
-};
 
 var Enemy = function(startX, startY, speed) {
     // Variables applied to each of our instances go here,
@@ -55,8 +36,11 @@ Enemy.prototype.update = function(dt) {
   var enemyYBottomMax = this.y + 65;
 
   if(player.x > enemyXleftMax && player.x < enemyXRightMax && player.y > enemyYTopMax &&        player.y < enemyYBottomMax) {
-    updateCount("fail", player);
+    //console.log("Passed count before is " + count);
+    count--;
+    //console.log("Passed count after is " + count);
     player.resetPlayer();
+    $("#count").text(count);
   }
 
 };
@@ -114,7 +98,12 @@ Player.prototype.handleInput = function(keyInput) {
     var player = this;
     if(this.y <= 100) {
       console.log("Reached top");
-      updateCount("pass", player);
+      count++;
+      if(count == 15) {
+        gameOver();
+      }
+      player.resetPlayer();
+      $("#count").text(count);
     }
     else {
       this.y -= moveUpDown;
@@ -173,7 +162,7 @@ var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-  console.log(e);
+  //console.log(e);
     var allowedKeys = {
         37: 'left',
         38: 'up',
